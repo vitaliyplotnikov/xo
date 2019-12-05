950b99bc2d865c0ac0dcafe7a02519f8d9d6a252
Vitaliy <vitaliyvplotnikov@gmail.com>
2019-12-05 15:34:24 +0200

    Added exceptions for setFigure and getFigure

--- src/model/Field.java
+++ src/model/Field.java
@@ -1,21 +1,47 @@
 package model;
+import model.exceptions.AlreadyOccupiedException;
+import model.exceptions.InvalidPointException;
+
 import java.awt.Point;
 
 public class Field {
 
     public static final int FIELD_SIZE = 3;
+    public static final int MIN_COORDINATE = 0;
+    public static final int MAX_COORDINATE = FIELD_SIZE;
     private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
     public int getSize() {
         return FIELD_SIZE;
     }
 
-    public Figure getFigure(Point point) {
+    public Figure getFigure(Point point) throws InvalidPointException {
+        if(!checkPoint(point)) {
+            throw new InvalidPointException();
+        }
+
         return field[point.x][point.y];
     }
 
-    public void setFigure (final Point point, final Figure figure) {
+    public void setFigure (final Point point, final Figure figure) throws InvalidPointException, AlreadyOccupiedException {
+
+        if(!checkPoint(point)) {
+            throw new InvalidPointException();
+        }
+
+        if (field[point.x][point.y] != null) {
+            throw new AlreadyOccupiedException();
+        }
+        
         field[point.x][point.y] = figure;
+    }
+
+    private boolean checkPoint(final Point point) {
+        return checkCoordinate(point.x) && checkCoordinate(point.y);
+    }
+
+    private boolean checkCoordinate(final int coordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate <= MAX_COORDINATE;
     }
 
 }
