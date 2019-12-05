2df5a6c3d788bc0b1da1360116bc6815f6a389c0
Vitaliy <vitaliyvplotnikov@gmail.com>
2019-12-05 16:27:37 +0200

    Added tests that check less or more coordinates then size of our field

--- tst/model/FieldTest.java
+++ tst/model/FieldTest.java
@@ -29,7 +29,46 @@ public class FieldTest {
         final Figure actualFigure = field.getFigure(inputPoint);
 
         assertEquals(inputFigure,actualFigure);
+    }
 
+    @Test
+    public void getFigureWhenXIsLessThenZero() throws AlreadyOccupiedException, InvalidPointException {
+        final Field field = new Field();
+        final Point inputPoint = new Point(-1,0);
 
+        try{
+            field.getFigure(inputPoint);
+            fail();
+        }catch(InvalidPointException e) {}
+    }
+    @Test
+    public void getFigureWhenYIsLessThenZero() throws AlreadyOccupiedException, InvalidPointException {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,-1);
+
+        try{
+            field.getFigure(inputPoint);
+            fail();
+        }catch(InvalidPointException e) {}
+    }
+    @Test
+    public void getFigureWhenXIsMoreThenSize() throws AlreadyOccupiedException, InvalidPointException {
+        final Field field = new Field();
+        final Point inputPoint = new Point(4,0);
+
+        try{
+            field.getFigure(inputPoint);
+            fail();
+        }catch(InvalidPointException e) {}
+    }
+    @Test
+    public void getFigureWhenYIsMoreThenSize() throws AlreadyOccupiedException, InvalidPointException {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,4);
+
+        try{
+            field.getFigure(inputPoint);
+            fail();
+        }catch(InvalidPointException e) {}
     }
 }
