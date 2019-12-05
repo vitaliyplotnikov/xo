9c0750b5b1709703807410519f8cc756e1cd1608
Vitaliy <vitaliyvplotnikov@gmail.com>
2019-12-05 13:08:02 +0200

    Created test PlayerTest

---
+++ tst/model/PlayerTest.java
@@ -0,0 +1,33 @@
+package model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void getName() {
+
+        final String inputValue = "Vitaliy";
+        final String expectedValue = inputValue;
+
+        final Player player = new Player(inputValue,null);
+        final String actualValue = player.getName();
+
+        assertEquals(expectedValue,actualValue);
+    }
+
+    @Test
+    public void getFigure() {
+
+        final Figure inputValue = Figure.X;
+        final Figure expectedValue = inputValue;
+
+        final Player player = new Player(null,inputValue);
+        final Figure actualValue = player.getFigure();
+
+        assertEquals(expectedValue,actualValue);
+
+    }
+}
